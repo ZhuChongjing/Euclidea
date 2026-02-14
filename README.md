d0fec185e7e1b8f5e3cf12f1154f3c28cd9a40b3
ZhuChongjing <zhuchongjing_pypi@163.com>
2026-02-14 10:00:55 +0800

    重新上传

---
+++ README.md
@@ -0,0 +1,5 @@
+![制作进度_Sheet1](https://github.com/user-attachments/assets/0de917cd-d184-4aea-87e7-b74fae2aeff3)
+
+<h1 align=center>Euclidea 解题参考</h1>
+
+在我寻找攻略的时候看到了[OrangeTown](https://github.com/OrangeTown)这位老哥的[Euclidea攻略](https://notepad.readthedocs.io/en/latest/euclidea/euclidea.html)，里面有着所有关卡的解析。可是里面只有最终的图案，没有步骤，我只能盯着那张最终的图片反复猜测，非常不方便。于是，我决定要自己做一个网站，上面有详细的解题步骤，并且可以通过点击来查看每一步。我便开始构思如何搭建这个网站，在这个过程中，我也发现了[MathsFans](https://githubcom/MathsFans)的[Euclidea解题参考](https://mathsfans.github.io/Euclidea)（详见[MathsFans/Euclidea](https://github.com/MathsFans/Euclidea)），它的初始页面让我眼前一亮，我便果断使用并更改了一些样式，**在这里感谢MathsFans**；中间的页面我直接用了[Euclidea官网](https://euclidea.xyz)的源代码以及图片；最后是每个关卡的页面，我保留了前面的样式，并进行了重新布局与样式修改，将官网绘图时的按钮放到页面中，点击并显示该步骤。顺带一提，我本来是准备使用[ManimCE](https://github.com/manimCommunity/manim)来制作解题动画的，但随着关卡的难度增加，动画的制作难度也变得更高，所以我还是选择了直接在网页画图并截取页面。至此，整个网站的框架就完成了，就等着我去截取接下来每一关的步骤了。**（教程关不提供解析）**
---
+++ level.js
@@ -0,0 +1,123 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const currentFilePath = window.location.pathname.split('/');
+    const currentFileName = currentFilePath.pop().replace(/\.[^/.]+$/, '');
+    
+    const currentStar = currentFileName.endsWith('_L' ) ? 'L' :
+                        currentFileName.endsWith('_E' ) ? 'E' :
+                        currentFileName.endsWith('_LE') ? 'LE' :
+                        currentFileName.endsWith('_V' ) ? 'V' : null;
+    document.querySelectorAll('.star-item').forEach(item => {
+        item.addEventListener('click', function() {
+            const starType = this.getAttribute('data-star');
+            const baseFileName = currentFileName.replace(/(_LE|_E|_L|_V)$/, '');
+            if (currentStar !== starType) {
+                window.location.href = `./${baseFileName}_${starType}.html`;
+            }
+        });
+    });
+    const starItem = document.querySelector(`.star-item[data-star="${currentStar}"]`);
+    if (starItem) {
+        starItem.classList.add('active');
+    }
+
+    document.querySelector('.solution-image').src = `/Euclidea/step-animation/${currentFileName}_0.png`;
+    document.querySelectorAll('.tool-button').forEach(button => {
+        button.addEventListener('click', function() {
+            document.querySelectorAll('.tool-button').forEach(b => b.classList.remove('active'));
+            this.classList.add('active');
+            
+            const step = parseFloat(this.getAttribute('step'));
+            const image = document.querySelector('.solution-image');
+            
+            if (image) {
+                image.src = `/Euclidea/step-animation/${currentFileName}_${step}.png`;
+            }
+        });
+    });
+
+    // TODO: 补全关卡名称
+    const levelName = {
+        // Alpha
+        'Angle60': ['1.1', 'Angle of 60°'],
+        'PerpBisector': ['1.2', 'Perpendicular Bisector'],
+        'Midpoint': ['1.3', 'Midpoint'],
+        'CircleInSquare': ['1.4', 'Circle in Square'],
+        'RhombusInRect': ['1.5', 'Rhombus in Rectangle'],
+        'CircleCenter': ['1.6', 'Circle Center'],
+        'SquareInCircle': ['1.7', 'Inscribed Square'],
+        // Beta
+        'BisectAngle': ['2.1', 'Angle Bisector'],
+        'Incenter': ['2.2', 'Intersection of Angle Bisectors'],
+        'Angle30': ['2.3', 'Angle of 30°'],
+        'DoubleAngle': ['2.4', 'Double Angle'],
+        'CutRectangle': ['2.5', 'Cut Rectangle'],
+        'DropPerp': ['2.6', 'Drop a Perpendicular'],
+        'ErectPerp': ['2.7', 'Erect a Perpendicular'],
+        'Tangent1': ['2.8', 'Tangent to Circle at Point'],
+        'CircleTangentL': ['2.9', 'Circle Tangent to Line'],
+        'CircleInRhombus': ['2.10', 'Circle in Rhombus'],
+        // Gamma
+        'ChordMidpoint': ['3.1', 'Chord Midpoint'],
+        'ATrByOrthocenter': ['3.2', 'Triangle by Angle and Orthocenter'],
+        'ATrByCircumcenter': ['3.3', 'Intersection of Perpendicular Bisectors'],
+        'AEqualSegments1': ['3.4', 'Three equal segments - 1'],
+        'CircleTangentPL': ['3.5', 'Circle through Point Tangent to Line'],
+        'TrapezoidCut': ['3.6', 'Midpoints of Trapezoid Bases'],
+        'Angle45': ['3.7', 'Angle of 45°'],
+        'Lozenge': ['3.8', 'Lozenge'],
+        'CentroidOfQuadrilateral': ['3.9', 'Centroid of Quadrilateral'],
+        // Delta
+        'CDoubleSeg': ['4.1', 'Double Segment'],
+        'Angle60Drop': ['4.2', 'Angle of 60° - 2'],
+        'EquilateralAboutCircle': ['4.3', 'Circumscribed Equilateral Triangle'],
+        'EquilateralInCircle': ['4.4', 'Equilateral Triangle in Circle'],
+        'Cut2Rectangles': ['4.5', 'Cut Two Rectangles'],
+        'Sqrt2': ['4.6', 'Square Root of 2'],
+        'Sqrt3': ['4.7', 'Square Root of 3'],
+        'Angle15': ['4.8', 'Angle of 15°'],
+        'SquareByOppMidpoints': ['4.9', 'Square by Opposite Midpoints'],
+        'SquareByAdjMidpoints': ['4.10', 'Square by Adjacent Midpoints'],
+        // Epsilon
+        'Parallel': ['5.1', 'Parallel Line'],
+        'Parallelogram3V': ['5.2', 'Parallelogram by Three Vertices'],
+        'LineAlongPoints': ['5.3', 'Line Equidistant from Two Points - 1'],
+        'LineBetweenPoints': ['5.4', 'Line Equidistant from Two Points - 2'],
+        'Hash': ['5.5', 'Hash'],
+        'ShiftAngle': ['5.6', 'Shift Angle'],
+        'EquidistantParallel': ['5.7', 'Line Equidistant from Two Lines'],
+        'SquareAboutCircle': ['5.8', 'Circumscribed Square'],
+        'SquareInSquare': ['5.9', 'Square in Square'],
+        'CircleInOutSquare': ['5.10', 'Circle Tangent to Square Side'],
+        'HexagonBySide': ['5.11', 'Regular Hexagon'],
+        // Zeta
+        'PtSymmetry': ['6.1', 'Point Reflection'],
+        'MirrorSeg': ['6.2', 'Line Reflection'],
+        'ShiftSegment': ['6.3', 'Shift Segment'],
+        'GivenAngleBisector': ['6.4', 'Given Angle Bisector'],
+        'CircleByRadius': ['6.5', 'Non-collapsing Compass'],
+        'TranslateSegment': ['6.6', 'Translate Segment'],
+        'TriangleBySides': ['6.7', 'Triangle by Three Sides'],
+        'ParallelogramBySP': ['6.8', 'Parallelogram'],
+        '9PointCircle': ['6.9', 'Nine Point Circle'],
+        '4SymmetricLines': ['6.10', 'Symmetry of Four Lines'],
+        'ParallelogramBy3Midpoints': ['6.11', 'Parallelogram by Three Midpoints'],
+    }
+    for (const level in levelName) {
+        if (currentFileName.startsWith(level)) {
+            document.head.querySelector('title').textContent = `Euclidea 解题参考 - ${levelName[level][1]}`;
+            document.querySelector('.level-title').textContent = levelName[level].join(' ');
+        }
+    }
+
+    const levelsList = [
+        'Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon',
+        'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa',
+        'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron'
+    ];
+    for (const pack of levelsList) {
+        if (currentFilePath.includes(pack)) {
+            document.querySelector('.button-back-icon').href = `/Euclidea/packs/${pack}.html`;
+            document.querySelector('.icon').classList.add(`svg-${pack}`);
+        }
+    }
+});
